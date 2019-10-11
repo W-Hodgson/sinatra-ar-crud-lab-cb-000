4b9eb6c8090b08dd2d172911266bb3ed5d78d495
Will Hodgson <will.hodgson@current-rms.com>
2019-10-11 14:25:50 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,47 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    erb :show
+  end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  post '/articles' do
+    new_article = Article.create(:title => params[:title], :content => params[:content])
+    new_article.save
+    redirect "/articles/#{new_article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id])
+
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    article = Article.find_by(id: params[:id])
+    article.title = params[:title]
+    article.content = params[:content]
+    article.save
+
+    redirect "/articles/#{params[:id]}"
+  end
+
+  delete '/articles/:id' do
+    article = Article.find_by(id: params[:id])
+    article.destroy
+    redirect '/articles'
+  end
 end
