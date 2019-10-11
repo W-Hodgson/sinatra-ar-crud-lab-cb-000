ce519136c02de004f68917fe90732cafbdd2a94d
Will Hodgson <will.hodgson@current-rms.com>
2019-10-11 14:07:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,6 +43,7 @@ class ApplicationController < Sinatra::Base
     article = Article.find_by(id: params[:id])
     article.title = params[:title]
     article.content = params[:content]
+    article.save
 
     redirect "/articles/#{params[:id]}"
   end
